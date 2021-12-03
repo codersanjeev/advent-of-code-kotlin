9089fafe5e9bd7c79c706cfe4f623ea9488b84ee
codersanjeev <thedevelopersanjeev@gmail.com>
2021-12-03 21:56:38 +0530

    day 03 puzzles done

---
+++ src/Day03A.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val submarineData = mutableListOf<String>()
+    while (true) {
+        val currentLine = readLine() ?: break
+        if (currentLine.isEmpty()) break
+        submarineData.add(currentLine)
+    }
+    var (gammaRate, epsilonRate) = listOf("", "")
+    for (columnIndex in submarineData[0].indices) {
+        var zerosCount = 0
+        var onesCount = 0
+        for (rowIndex in submarineData.indices) {
+            if (submarineData[rowIndex][columnIndex] == '0') {
+                ++zerosCount
+            } else {
+                ++onesCount
+            }
+        }
+        if (onesCount > zerosCount) {
+            gammaRate += "1"
+            epsilonRate += "0"
+        } else {
+            gammaRate += "0"
+            epsilonRate += "1"
+        }
+    }
+    println(gammaRate.toInt(2) * epsilonRate.toInt(2))
+}
---
+++ src/Day03B.kt
@@ -0,0 +1,40 @@
+fun main() {
+    val submarineData = mutableListOf<String>()
+    while (true) {
+        val currentLine = readLine() ?: break
+        if (currentLine.isEmpty()) break
+        submarineData.add(currentLine)
+    }
+    val dataWidth = submarineData[0].length
+    var oxygenData = submarineData.toMutableList()
+    var carbonData = submarineData.toMutableList()
+    for (columnIndex in 0 until dataWidth) {
+        if (oxygenData.size > 1) {
+            val zeros = oxygenData.count { it[columnIndex] == '0' }
+            val ones = oxygenData.count { it[columnIndex] == '1' }
+            oxygenData = if (ones >= zeros) {
+                oxygenData.filter {
+                    it[columnIndex] == '1'
+                } as MutableList<String>
+            } else {
+                oxygenData.filter {
+                    it[columnIndex] == '0'
+                } as MutableList<String>
+            }
+        }
+        if (carbonData.size > 1) {
+            val zeros = carbonData.count { it[columnIndex] == '0' }
+            val ones = carbonData.count { it[columnIndex] == '1' }
+            carbonData = if (ones >= zeros) {
+                carbonData.filter {
+                    it[columnIndex] == '0'
+                } as MutableList<String>
+            } else {
+                carbonData.filter {
+                    it[columnIndex] == '1'
+                } as MutableList<String>
+            }
+        }
+    }
+    println(oxygenData[0].toInt(2) * carbonData[0].toInt(2))
+}
